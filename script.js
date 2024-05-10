d8c8cc984cb705c75e9fc321cb66582fe1a21e7b
yk-david <81497136+yk-david@users.noreply.github.com>
2024-05-10 14:40:12 +0000

    Make code DRY with defining even handler function

--- script.js
+++ script.js
@@ -6,21 +6,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+const openModal = function() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function() {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', function() {
-  console.log('Button clicked');
+const closeModal = function() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-})
+}
 
-overlay.addEventListener('click', function() {
-  console.log('Clicked');
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-})
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
